b11f5c7fe9f2afe2d43446161b7da936ee69c2c2
aashishword004-alt <aashishword004@gmail.com>
2026-08-17 22:27:48 +0530

    done with py

---
+++ pattern.py
@@ -0,0 +1,20 @@
+i=0
+j=0
+k=0
+
+for i in range(1,6):
+    print(i)
+print("///////////////////////////")
+for i in range(1,6):
+    for j in range(i):
+        print(j,end="")
+    print()
+print("///////////////////////////")
+
+for i in range(1,6):
+    for j in range(i,6):
+         print(j,end="")
+    print()
+
+print("///////////////////////////")
+
