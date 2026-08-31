93f6648575631aff44d149481b9afd5bc270983d
Ashishworld <aashishword004@gmail.com>
2026-08-31 14:42:08 +0530

    Fix vowel counting logic in tapul_1.py
    Count the number of vowels in the user input and print the total.

--- tapul_1.py
+++ tapul_1.py
@@ -7,15 +7,14 @@ This is a temporary script file.
 ''' 1.	Take a string as an input from the user.
  Find total number of vowels in it. (Hint: take a tuple of vowels)
 '''
-
 inn = tuple(input('Enter the input '))
 vowel = ('a','i','u','e')
 
-total_number = 0
-
-for i in vowel:
-    total_number += 1
-    print(i)
+total = 0
 
 
-print(total_number,'are vowel in input ')
+for i in inn:
+    if i in vowel:
+        total+=1
+        print(total,'are vowel number ')
+        
