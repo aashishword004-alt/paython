381107259ecfb0701cf1fee18564810ef86aad5d
aashishword004-alt <aashishword004@gmail.com>
2026-09-22 00:19:21 +0530

    done 2 question

--- class/first.py
+++ class/first.py
@@ -4,7 +4,6 @@
 4.	Reverse the given number and display the sum of its digits.
 '''
 #1.	Count and display the number of vowels, consonants and digits present in the string. 
-
 user = input("Enter the input : ")
 
 cout = 0
@@ -23,6 +22,41 @@ for char in user:
 
 print(is_vowel,cout)
 print(num)
-print(consonats)
-
+print(consonats) 
 # 2.	Display all the words from the string that are palindromes. 
+string = str(input('Enter the input  :'))
+palind = False
+if string == string[::-1]:
+        palind = True
+
+if palind == False:
+    print(string , 'is not palindromes')
+else:
+    print(string,'is palindromes') 
+    
+# 3.	Check whether the given number is an Armstrong number or not.
+n = int(input('Enter The input : '))
+temp = n 
+length = len(str(n))
+total = 0
+while temp > 0:
+    digit = temp % 10
+    total += digit ** length
+    temp = temp // 10
+
+if n == total:
+    print(n ,' is armstrong number ') 
+else:
+     print(n ,' is not armstrong number ') 
+
+# 4.	Reverse the given number and display the sum of its digits.
+n  = input('Enter the number : ')
+rev = n[::-1]
+digit = int(n)
+total = 0
+while digit > 0:
+    i = digit % 10
+    total += i
+    digit = digit // 10
+print(rev)
+print(total)
---
+++ class/second.py
@@ -0,0 +1,40 @@
+'''Q- 2 Write a Python program to input n positive integers from the user and store them in a list. Perform the following tasks:
+1.	Create a dictionary having each number as the key and its sum of digits as the value. 
+2.	Create a tuple containing all the palindrome numbers from the given list. 
+3.	Create a set containing all the unique digits occurring in the given numbers. 
+4.	Find the number having the highest sum of digits. If more than one number has the same highest digit sum, display all such numbers. 
+5.	Display all numbers which are both palindrome and divisible by the sum of their digits.
+'''
+
+# 1.	Create a dictionary having each number as the key and its sum of digits as the value.
+'''
+num = int(input('Enter the Number : '))
+d = {}
+for i in range(num):
+ n = int(input('Enter the Value : '))
+ temp = n 
+ total = 0
+ while n > 0:
+  ia = n % 10
+  total +=ia
+  n = n // 10
+ d[temp] = {total}
+
+print(d)
+
+'''
+# 2.	Create a tuple containing all the palindrome numbers from the given list. 
+
+l1 = [123,153,154,151,161,717]
+l2 = []
+for i in l1:
+  newstr = str(i)
+  if newstr == newstr[::-1]:
+     l2.append(newstr)
+
+num = list(map(int,l2))
+
+pali = tuple(num)
+print(pali)
+
+
